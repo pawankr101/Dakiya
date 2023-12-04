979af32d8f0809c0b017d2d85a8a9975a8ddd8f7
Akshay Kumar <pawan.akshaykr@gmail.com>
2023-12-04 08:27:31 +0530

    new changes

---
+++ client/src/app/services/worker.ts
@@ -0,0 +1,25 @@
+import { Exception } from "../../exceptions"
+
+type WorkerMessageDataInput = {
+    method: string,
+    arg: any[]
+}
+type WorkerMessageDataOutput<T=any> = {
+    error: Exception,
+    output: T
+}
+const worker = new Worker(new URL("../../workers/index", import.meta.url), {name: 'worker'});
+
+export async function callWorkerMethod<T>(method: string, arg: any[]) {
+    return new Promise((resolve: (v: T)=>void, reject) => {
+        if(method) {
+            worker.onmessage = (event: MessageEvent<WorkerMessageDataOutput<T>>) => {
+                if(event.data?.error) reject(new Exception(event.data.error));
+                else if(event.data?.output) resolve(event.data.output);
+                else reject(new Exception(`Message from ${worker}`));
+            }
+            worker.postMessage({method, arg});
+        }
+    })
+}
+
---
+++ server/src/app/app.route.ts
@@ -0,0 +1,5 @@
+
+
+export function AppRoutes(fastify, options, done) {
+
+}
