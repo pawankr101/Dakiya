4acc6a51e894463f75c61806e51156f43ce39a98
Akshay Kumar <pawan.akshaykr@gmail.com>
2026-02-19 09:23:40 +0530

    updated DB Driver

---
+++ server/src/storage/DB/pg/connection.ts
@@ -0,0 +1,9 @@
+import { default as postgres } from 'postgres';
+
+export const DB_CONNECTION = postgres({
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432'),
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
