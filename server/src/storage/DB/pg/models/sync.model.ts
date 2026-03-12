4057920ad3f0649e8c41c46522e941dd6a7aee0d
Akshay Kumar <pawan.akshaykr@gmail.com>
2026-03-12 23:52:29 +0530

    added postgresql related apis

--- server/src/storage/DB/pg/models/sync.model.ts
+++
@@ -1,7 +0,0 @@
-export interface DeliveryQueueItem {
-    uid: string;
-    recipientDeviceId: string;
-    deliveryItemType: 'message' | 'reaction' | 'edit' | 'read_receipt';
-    deliveryItemId: string; // FK to the item being delivered
-    createdAt: Date;
-}
--- server/src/storage/DB/pg/repositories/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './identity.repository.js';
